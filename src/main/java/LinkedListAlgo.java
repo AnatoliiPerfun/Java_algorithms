5638190c8e5d9fa047ce920aec00773df9d058af
Anatolii Perfun <tolikvik@gmail.com>
2022-05-17 21:57:16 +0300

    one more solution added to array and string. LinkedList algo added

--- src/main/java/LinkedListAlgo.java
+++ src/main/java/LinkedListAlgo.java
@@ -1,8 +1,26 @@
+
 import java.util.LinkedList;
 
 public class LinkedListAlgo {
 
     public static void main(String[] args) {
-        LinkedList<String> linkedList = new LinkedList<>();
+        LinkedList<String> languages = new LinkedList<>();
+        // add elements in LinkedList
+        languages.add("Java");
+        languages.add("Python");
+        languages.add("Typescript");
+        languages.add("C#");
+        languages.add("DART");
+
+        System.out.println("LinkedList: " + languages);
+        // get the element from the linked list
+        String str = languages.get(2);
+        System.out.print("Element at index 1: " + str);
+        // remove elements from index 1
+        String string = languages.remove(1);
+        // change elements at index 3
+        languages.set(3, "Kotlin");
+        System.out.println("Removed Element: " + string);
+        System.out.println("Updated LinkedList: " + languages);
     }
 }
